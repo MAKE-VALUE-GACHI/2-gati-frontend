32bcf2ce96dd4edc744e96ce81c6068abc51188e
DOEUN <ide05484@gmail.com>
2025-07-12 14:33:34 +0900

    feat: vite pwa 기본 세팅

--- Gati_Frontend/vite.config.ts
+++ Gati_Frontend/vite.config.ts
@@ -1,8 +1,48 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "한끼하자", // 설치 배너에 표시되는 이름
+        short_name: "한끼하자", // 아이콘 아래에 표시될 이름
+        description: "팀 가티의 프로젝트, 한끼하자 입니다.",
+        lang: "ko",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
